6f595efb31e480c3e2be2e46f51d78ac9aa67f80
Jael <jaele.jones@gmail.com>
2016-07-13 09:09:02 -0400

    Added Basic Questions part 3 and 4

--- selectbasics.sql
+++ selectbasics.sql
@@ -306,3 +306,151 @@ Guatemala
 Kuwait
 Mexico
 Panama
+
+-- # 1. List each country name where the population is larger than
+-- # 'Russia'.
+SELECT name
+FROM world
+WHERE population > (
+  SELECT population
+  FROM world
+  WHERE name = 'Russia');
+
+-- # 2. List the name and continent of countries in the continents
+-- # containing 'Belize', 'Belgium'.
+SELECT name, continent
+FROM world
+WHERE continent IN (
+  SELECT continent 
+  FROM world
+  WHERE name IN ('Belize', 'Belgium'));
+
+-- # 3. Show the countries in Europe with a per capita GDP greater than
+-- # 'United Kingdom'.
+SELECT name
+FROM world
+WHERE continent = 'Europe'
+AND gdp/population > (
+  SELECT gdp/population
+  FROM world
+  WHERE name = 'United Kingdom');
+
+-- # 4. Which country has a population that is more than Canada but
+-- # less than Poland? Show the name and the population.
+SELECT name, population
+FROM world
+WHERE population > (
+  SELECT population
+  FROM world
+  WHERE name = 'Canada')
+AND population < (
+  SELECT population
+  FROM world
+  WHERE name = 'Poland');
+
+-- # 5. Which countries have a GDP greater than any country in Europe?
+-- # [Give the name only.]
+SELECT name
+FROM world
+WHERE gdp > (
+  SELECT MAX(gdp)
+  FROM world
+  WHERE continent = 'Europe');
+
+-- # 6. Find the largest country (by area) in each continent, show the 
+-- # continent, the name and the area.
+SELECT x.continent, x.name, x.area
+FROM world AS x
+WHERE x.area = (
+  SELECT MAX(y.area)
+  FROM world AS y
+  WHERE x.continent = y.continent)
+
+-- # 7. Find each country that belongs to a continent where all
+-- # populations are less than 25000000. Show name, continent and
+-- # population.
+SELECT x.name, x.continent, x.population
+FROM world AS x
+WHERE 25000000 > ALL (
+  SELECT y.population
+  FROM world AS y
+  WHERE x.continent = y.continent);
+
+-- # 8. Some countries have populations more than three times that of
+-- # any of their neighbours (in the same continent). Give the
+-- # countries and continents.
+SELECT x.name, x.continent
+FROM world AS x
+WHERE x.population/3 > ALL (
+  SELECT y.population
+  FROM world AS y
+  WHERE x.continent = y.continent
+  AND x.name != y.name);
+
+--1. Change the query shown so that it displays Nobel prizes for 1950.
+
+SELECT yr, subject, winner
+  FROM nobel
+ WHERE yr = 1950;
+
+ --2. Show who won the 1962 prize for Literature.
+
+SELECT winner
+  FROM nobel
+ WHERE yr = 1962
+   AND subject = 'Literature';
+
+--3. Show the year and subject that won 'Albert Einstein' his prize.
+
+SELECT yr, subject FROM nobel WHERE winner = 'Albert Einstein';
+
+--4. Give the name of the 'Peace' winners since the year 2000, including 2000.
+
+SELECT winner FROM nobel WHERE subject = 'Peace' AND yr >= 2000;
+
+--5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+
+SELECT * FROM nobel WHERE subject = 'Literature' AND yr >= 1980 AND yr <= 1989;
+
+--6. Show all details of the presidential winners
+
+SELECT * FROM nobel
+ WHERE winner IN ('Theodore Roosevelt',
+                  'Woodrow Wilson',
+                  'Jimmy Carter');
+
+--7. Show the winners with first name John
+
+SELECT winner FROM nobel WHERE winner LIKE 'John%';
+
+--8. Show the Physics winners for 1980 together with the Chemistry winners for 1984.
+
+SELECT * FROM nobel WHERE subject = 'Physics' AND yr = 1980 OR subject = 'Chemistry' AND yr = 1984;
+
+--9. Show the winners for 1980 excluding the Chemistry and Medicine
+
+SELECT * FROM nobel WHERE yr = 1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+--10. Show who won a 'Medicine' prize in an early year (before 1910, not including 1910) together with winners of a 'Literature' prize in a later year (after 2004, including 2004)
+
+SELECT * FROM nobel WHERE subject = 'Medicine' AND yr < 1910 OR subject = 'Literature' AND yr >= 2004;
+
+--11. Find all details of the prize won by PETER GRÜNBERG
+
+SELECT * FROM nobel WHERE winner = 'Peter Grünberg';
+
+--12.Find all details of the prize won by EUGENE O'NEILL
+
+SELECT * FROM nobel WHERE winner = 'Eugene O''Neill';
+
+--13. List the winners, year and subject where the winner starts with Sir. Show the the most recent first, then by name order.
+
+Select winner, yr, subject FROM nobel WHERE winner LIKE 'Sir%' ORDER BY yr DESC, winner;
+
+--14. Show the 1984 winners ordered by subject and winner name; but list Chemistry and Physics last.
+
+SELECT winner, subject, subject IN ('Physics','Chemistry')
+  FROM nobel
+ WHERE yr=1984
+ ORDER BY subject IN ('Physics','Chemistry'), subject,winner;
+ 
